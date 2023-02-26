dbaca74d915d6d8dd395500809aeec93524e9f0c
Jordi Turull <jordi.frontend@gmail.com>
2023-02-27 00:32:06 +0100

    feat: chron job

--- sh/getJSON.js
+++ sh/getJSON.js
@@ -7,13 +7,6 @@ var timezone = require("dayjs/plugin/timezone");
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
-// Make the following curl call to get the JSON from the API
-// curl 'https://api.happergy.es/bestMomentDevices' \
-//  -H 'Content-Type: application/json' \
-//  --data-raw '{"config":[{"id":"generic1","duration":1},{"id":"generic2","duration":2},{"id":"generic4","duration":4},{"id":"generic6","duration":6},{"id":"washing","name":"washing","category":"washing","duration":2,"consumptionKWh":0.82,"showStartTime":true},{"id":"dishwasher","category":"dishwasher","name":"Lavavajillas","consumptionKWh":0.95,"customLabel":true,"duration":1,"timesPerMonth":null,"rangeStart":0,"rangeEnd":1,"showStartTime":true},{"id":"oven","name":"oven","category":"oven","duration":1,"consumptionKWh":0.81,"rangeStart":11,"rangeEnd":15,"showStartTime":true},{"id":"1644020579294","category":"charger","name":"BMW i3","consumptionKWh":10,"customLabel":true,"duration":8,"timesPerMonth":null,"rangeStart":0,"rangeEnd":0,"showStartTime":true},{"id":"1654899947976","category":"logo","name":"Name","consumptionKWh":0.82,"customLabel":true,"duration":4,"timesPerMonth":null,"rangeStart":22,"rangeEnd":8,"showStartTime":true}],"tariff":"PCB"}' \
-//  --compressed
-
-// make a post request to the API with an object containing the config and tariff
 request.post(
   "https://api.happergy.es/bestMomentDevices",
   {
@@ -51,17 +44,9 @@ request.post(
           if (err) {
             return console.log(err);
           }
-          console.log("The file was saved!");
+          console.log("The file" + now + ".json was saved!");
         }
       );
-
-      // write the response body to a file
-      // fs.writeFile("bestMomentDevices.json", body, function (err) {
-      //   if (err) {
-      //     return console.log(err);
-      //   }
-      //   console.log("The file was saved!");
-      // });
     }
   }
 );
