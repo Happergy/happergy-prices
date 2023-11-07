55f384f1cea04107cbb3c2c4b71e78e458652287
Marc Ferret <marc.ferret@mango.com>
2023-11-07 19:55:14 +0100

    feat: send notification and save parsed pvpc

--- sh/pvpc.js
+++ sh/pvpc.js
@@ -26,7 +26,7 @@ try {
     console.log(`[PVPC] The file exists: ${targetFilePath}`);
   } else {
     request.get(
-      `https://us-central1-best-price-pvpc.cloudfunctions.net/getTomorrowPricesPVPC?sendMessage=false&date=2023-11-06`,
+      `https://us-central1-best-price-pvpc.cloudfunctions.net/getTomorrowPricesPVPC`,
       {},
       function (error, response, data) {
         if (error) {
